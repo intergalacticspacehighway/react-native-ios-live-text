e1f50fdb6c90024d1d280eade90686029c22775a
Nishan <nishanbende@gmail.com>
2022-07-14 11:05:20 +0530

    init

--- ios/LiveTextImageViewViewManager.swift
+++ ios/LiveTextImageViewViewManager.swift
@@ -4,8 +4,6 @@ import Vision
 
 @objc(LiveTextImageViewViewManager)
 class LiveTextImageViewViewManager: RCTViewManager {
-    
-
     override func view() -> (UIView) {
         if #available(iOS 16.0, *) {
             return LiveTextImageViewView()
@@ -13,79 +11,57 @@ class LiveTextImageViewViewManager: RCTViewManager {
             return UIView()
             // Fallback on earlier versions
         }
-  }
-
-  @objc override static func requiresMainQueueSetup() -> Bool {
-    return false
-  }
+    }
+    
+    @objc override static func requiresMainQueueSetup() -> Bool {
+        return false
+    }
 }
 
 @available(iOS 16.0, *)
 class LiveTextImageViewView : UIView {
-  
-  
-
     private lazy var interaction: ImageAnalysisInteraction = {
         let interaction = ImageAnalysisInteraction()
         interaction.preferredInteractionTypes = .automatic
         return interaction
     }()
-
+    
     private let imageAnalyzer = ImageAnalyzer()
     
     @objc var enabled: Bool = false {
         didSet {
             if (enabled) {
-            
-            if let image = self.subviews.first as? RCTImageView {
                 
-                let imageView = image.imageView;
-                imageView?.addInteraction(interaction)
-                guard let image = imageView?.image else {
-                    return
-                }
-                
-                Task {
-                    let configuration = ImageAnalyzer.Configuration([.text])
+                if let image = self.subviews.first as? RCTImageView {
                     
-                    do {
-                        let analysis = try await imageAnalyzer.analyze(image, configuration: configuration)
+                    let imageView = image.imageView;
+                    imageView?.addInteraction(interaction)
+                    guard let image = imageView?.image else {
+                        return
+                    }
+                    
+                    Task {
+                        let configuration = ImageAnalyzer.Configuration([.text])
                         
-                        DispatchQueue.main.async {
-                            self.interaction.analysis = nil
-                            self.interaction.preferredInteractionTypes = []
+                        do {
+                            let analysis = try await imageAnalyzer.analyze(image, configuration: configuration)
                             
-                            self.interaction.analysis = analysis
-                            self.interaction.preferredInteractionTypes = .automatic
+                            DispatchQueue.main.async {
+                                self.interaction.analysis = nil
+                                self.interaction.preferredInteractionTypes = []
+                                
+                                self.interaction.analysis = analysis
+                                self.interaction.preferredInteractionTypes = .automatic
+                            }
+                            
+                        } catch {
+                            print(error.localizedDescription)
                         }
-                        
-                    } catch {
-                        print(error.localizedDescription)
                     }
                 }
+                
             }
-           
         }
     }
-}
-  
     
-    
-    
-
-  func hexStringToUIColor(hexColor: String) -> UIColor {
-    let stringScanner = Scanner(string: hexColor)
-
-    if(hexColor.hasPrefix("#")) {
-      stringScanner.scanLocation = 1
-    }
-    var color: UInt32 = 0
-    stringScanner.scanHexInt32(&color)
-
-    let r = CGFloat(Int(color >> 16) & 0x000000FF)
-    let g = CGFloat(Int(color >> 8) & 0x000000FF)
-    let b = CGFloat(Int(color) & 0x000000FF)
-
-    return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1)
-  }
 }
